7e9055d521cabeeb6d2ca63a43e227a95180ce44
Michael Bryan <michaelfbryan@gmail.com>
2017-07-31 10:46:24 +0800

    Updated the callback example a bit

--- src/callbacks/main.c
+++ src/callbacks/main.c
@@ -5,5 +5,5 @@ char safe_get_item_10000(char *buffer, size_t len);
 
 int main() {
   char buffer[50] = {};
-  char got = get_item_10000(buffer, 50);
+  char got = safe_get_item_10000(buffer, 50);
 }
