d21caf5e9c5f94dc24ae1de8952e8da130a07386
Michael Bryan <michaelfbryan@gmail.com>
2018-06-30 21:51:25 +0800

    More tweaks

--- ci/test.sh
+++ ci/test.sh
@@ -2,7 +2,8 @@
 
 set -ex
 
-PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_DIR=$(dirname "$SCRIPT_DIR")
 
 cd $PROJECT_DIR && pwd && ls
 make build
